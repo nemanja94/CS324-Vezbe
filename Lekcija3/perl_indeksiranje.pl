7dac75ca9d323772c46bb0a7e307b40b45f1b8a5
nemanja94 <aandric.nemanja@gmail.com>
2017-07-10 11:12:03 +0200

    Dodati komentari

--- Lekcija3/perl_indeksiranje.pl
+++ Lekcija3/perl_indeksiranje.pl
@@ -1,17 +1,23 @@
 #!/usr/bin/perl
 
+#Kreiranje niza i dodavanje skalarnih vrednosti
 @coins = ("Quarter", "Dime", "Nickel");
 
+#formatiranje ispisa
 print "\n";
 
+#Prikaz svih elemenata niza
 print "@coins\n";
 
+#Prikaz pojedinacnih elementa pomocu indeksa
 print $coins[0];
 print "\n";
 
+#Prikaz pojedinacnih elementa pomocu indeksa
 print $coins[1];
 print "\n";
 
+#Prikaz pojedinacnih elementa pomocu indeksa
 print $coins[2];
 print "\n";
 
--- Lekcija3/perl_niz.pl
+++ Lekcija3/perl_niz.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
 
+#Kreiranje niza i dodavanje skalarnih vrednosti
 @coins = ("Quarter", "Dime", "Nickel");
 
+#Nekoliko nacina da se istampaju vrednosti niza
 print "@coins\n";
 print @coins;
 print "\n";
