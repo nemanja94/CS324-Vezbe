d848dee0c9ef78b6a93756c40dd85c0accb24006
nemanja94 <aandric.nemanja@gmail.com>
2017-07-10 11:23:54 +0200

    Komentari izmenjani

--- Lekcija3/perl_niz.pl
+++ Lekcija3/perl_niz.pl
@@ -3,7 +3,7 @@
 #Kreiranje niza i dodavanje skalarnih vrednosti
 @coins = ("Quarter", "Dime", "Nickel");
 
-#Nekoliko nacina da se istampaju vrednosti niz
+#Nekoliko nacina da se istampaju vrednosti niza
 print "@coins\n";
 print @coins;
 print "\n";
