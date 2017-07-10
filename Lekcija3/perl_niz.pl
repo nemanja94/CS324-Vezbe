f919010ab23688f23433eef887a60440679a02f6
nemanja94 <aandric.nemanja@gmail.com>
2017-07-10 11:53:11 +0200

    Dodati komentari

--- Lekcija3/perl_niz.pl
+++ Lekcija3/perl_niz.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
 
+#Kreiranje niza i dodavanje skalar promenljivih
 @coins = ("Quarter", "Dime", "Nickel");
 
+#Par nacina da se prikaze sadrzaj niza
 print "@coins\n";
 print @coins;
 print "\n";
