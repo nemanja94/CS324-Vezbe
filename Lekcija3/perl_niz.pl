9f7d844c60cdd9f78b1f695cacca72fa6ef3dc93
nemanja94 <aandric.nemanja@gmail.com>
2017-07-10 11:12:03 +0200

    Dodati komentari

--- Lekcija3/perl_niz.pl
+++ Lekcija3/perl_niz.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
 
+#Kreiranje niza i dodavanje skalarnih vrednosti
 @coins = ("Quarter", "Dime", "Nickel");
 
+#Nekoliko nacina da se istampaju vrednosti niz
 print "@coins\n";
 print @coins;
 print "\n";
